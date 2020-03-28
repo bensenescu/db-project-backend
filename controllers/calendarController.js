2fd7f3ce55819614b35f34596b3367726db5e030
Gianluca Zigni <gianlucazigni@gmail.com>
2020-03-28 15:08:48 -0400

    Added Google Cloud Connectivity

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -1,10 +1,16 @@
+const fs = require('fs');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '34.73.147.161',
   user: 'root',
-  password: 'password',
+  password: 'password1',
   database: 'calendar',
+  ssl: {
+    ca: fs.readFileSync(`${process.cwd()}/certs/server-ca.pem`),
+    key: fs.readFileSync(`${process.cwd()}/certs/client-key.pem`),
+    cert: fs.readFileSync(`${process.cwd()}/certs/client-cert.pem`),
+  },
 });
 
 async function getSections(req, res) {
