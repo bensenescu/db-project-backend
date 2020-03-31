2980d67991fbe3659e02cda18a10c2cc559b8370
nikdmello <nik.dmello@gmail.com>
2020-03-31 19:54:31 +0400

    Add calendarItem API methods

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -78,10 +78,93 @@ async function createUser(req, res) {
   });
 }
 
+async function createCourse(req, res) {
+  const course = req.body;
+  const sql = 'INSERT INTO courses SET ?';
+
+  connection.query(sql, course, (error, results) => {
+    if (error) {
+      res.send({ error });
+    } else {
+      res.send(results);
+    }
+  });
+}
+
+async function deleteCourse(req, res) {
+  const { courseId } = req.body;
+  const sql = 'DELETE FROM courses WHERE courseId = ?';
+
+  connection.query(sql, courseId, (error, results) => {
+    if (error) {
+      res.send({ error });
+    } else {
+      res.send(results);
+    }
+  });
+}
+
+async function getCourses(req, res) {
+  const sql = 'SELECT * FROM courses';
+
+  connection.query(sql, (error, results) => {
+    if (error) {
+      res.send({ error });
+    } else {
+      res.send(results);
+    }
+  });
+}
+
+async function createCalendarItem(req, res) {
+  const calendarItem = req.body;
+  const sql = 'INSERT INTO calendarItems SET ?';
+
+  connection.query(sql, calendarItem, (error, results) => {
+    if (error) {
+      res.send({ error });
+    } else {
+      res.send(results);
+    }
+  });
+}
+
+async function getCalendarItems(req, res) {
+  const sql = 'SELECT * FROM calendarItems';
+
+  connection.query(sql, (error, results) => {
+    if (error) {
+      res.send({ error });
+    } else {
+      res.send(results);
+    }
+  });
+}
+
+async function deleteCalendarItem(req, res) {
+  const { itemId } = req.body;
+  const sql = 'DELETE FROM calendarItems WHERE itemId = ?';
+
+  connection.query(sql, itemId, (error, results) => {
+    if (error) {
+      res.send({ error });
+    } else {
+      res.send(results);
+    }
+  });
+}
+
+
 module.exports = {
   getSections,
   createSection,
   deleteSection,
   getUser,
   createUser,
+  createCourse,
+  deleteCourse,
+  getCourses,
+  createCalendarItem,
+  getCalendarItems,
+  deleteCalendarItem
 };
--- routes/calendarRouter.js
+++ routes/calendarRouter.js
@@ -14,4 +14,14 @@ router.route('/users')
 router.route('/users/:email')
   .get(calendarController.getUser);
 
+router.route('/courses')
+  .post(calendarController.createCourse)
+  .get(calendarController.getCourses)
+  .delete(calendarController.deleteCourse);
+
+router.route('/calendarItems')
+  .post(calendarController.createCalendarItem)
+  .get(calendarController.getCalendarItems)
+  .delete(calendarController.deleteCalendarItem);
+
 module.exports = router;
